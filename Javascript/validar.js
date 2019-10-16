9c186992efa9141da2ca13373321689b02b76792
having24 <javieremg4@gmail.com>
2019-10-16 13:24:22 -0500

    Formulario de registro de alumno terminado (falta validar CURP)

--- Javascript/validar.js
+++ Javascript/validar.js
@@ -1,14 +1,4 @@
 function validar(){
-	var fnac = document.getElementById('fnac').value;
-	if(fnac===""){
-		alert("Seleccione la Fecha de Nacimiento");
-		return false;
-	}
-	var direccion = document.getElementById('direccion').value;
-	if(direccion===""){
-		alert("Agregue una Dirección");
-		return false;
-	}
 	var curp = document.getElementById('curp').value;
 	if(!(curp.length===18 || curp==="")){
 		alert("Error: Revise la CURP");
@@ -29,13 +19,57 @@ function validar(){
 	}
 	/*if(!apepat.search(expLetras) && !apemat.search(expLetras) && !nombre.search(expLetras) && nombre!=""){
 		alert("Nombre correcto");
-	}*/
+	}
 	var expNumeros = /^\d*$/;
 	var telcel = document.getElementById('telcel').value;
 	var telcasa = document.getElementById('telcasa').value;
 	if(telcel.search(expNumeros) || telcasa.search(expNumeros) || telcel.length>15 || telcel.length>15){
 		alert("Error: Revise los telefonos");
 		return false;
+	}*/
+	var fnac = document.getElementById('fnac').value;
+	if(fnac===""){
+		alert("Seleccione la Fecha de Nacimiento");
+		return false;
+	}
+	if(!validarFormatoFecha(fnac) || !existeFecha(fnac) || !validarFechaMenorActual(fnac)){
+		alert("Error: Revise la Fecha de Nacimiento");
+		return false;
+	}
+	var finscrip = document.getElementById('finscrip').value;
+	if(!validarFormatoFecha(finscrip) || !existeFecha(finscrip) || !validarFechaMenorActual(finscrip)){
+		alert("Error: Revise la Fecha de Inscripcion");
+		return false;
+	}
+	var direccion = document.getElementById('direccion').value;
+	if(direccion===""){
+		alert("Agregue una Dirección");
+		return false;
 	}
 	return true;
+}
+function validarFormatoFecha(campo) {
+	var RegExPattern = /^\d{2,4}\-\d{1,2}\-\d{1,2}$/;
+	if ((campo.match(RegExPattern)) && (campo!='')) {
+	      return true;
+	} else {
+	      return false;
+	}
+}
+function existeFecha (fecha) {
+        var fechaf = fecha.split("-");
+        var d = fechaf[2];
+        var m = fechaf[1];
+        var y = fechaf[0];
+        return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
+}
+function validarFechaMenorActual(date){
+	var x=new Date();
+	var fecha = date.split("-");
+	x.setFullYear(fecha[0],fecha[1]-1,fecha[2]);
+	var today = new Date();
+	if (x > today)
+		return false;
+	else
+		return true;
 }
--- PHP/registroAlumno.php
+++ PHP/registroAlumno.php
@@ -1,19 +1,95 @@
 <?php
-	$uploadedfileload=true;
-	$uploadedfile_size=$_FILES['uploadedfile']['size'];
-	$msg="";
-	if ($_FILES['uploadedfile']['size']>200000)
-	{
-		$msg="El archivo es mayor que 200KB <br>";
+	//Aquí se valida si se subio una foto
+	if($_FILES['uploadedfile']['size']==0){
 		$uploadedfileload=false;
+		echo "<h3 style='color: red;'>No agregó foto</h3>";
+	//Fin
+	}else{
+		//Aquí se valida que la imagen/foto tenga las caracteristicas correspondientes (fomato: jpg o png, peso<=200KB)
+		$uploadedfileload=true;
+		$msg="";
+		if ($_FILES['uploadedfile']['size']>200000){
+			$msg="El archivo es mayor que 200KB <br>";
+			$uploadedfileload=false;
+		}
+		if (!($_FILES['uploadedfile']['type'] =="image/jpeg" OR $_FILES['uploadedfile']['type'] =="image/png")){
+			$msg=$msg."El archivo tiene que ser JPG o PNG";
+			$uploadedfileload=false;
+		}
+		if(!$uploadedfileload){
+			echo "<h3 style='color: red;'>".$msg."</h3>";
+			echo "<a href='../HTML/registroAlumno.html'>Volver al registro</a>";
+		}
+		//Fin
 	}
-	if (!($_FILES['uploadedfile']['type'] =="image/jpeg" OR $_FILES['uploadedfile']['type'] =="image/png"))
-	{
-		$msg=$msg."El archivo tiene que ser JPG o PNG";
-		$uploadedfileload=false;
-	}
-	if(!$uploadedfileload){
-		echo "<h4 style='color: red;'>".$msg."</h4>";
+	$curp = $_POST['curp'];
+	$nombre = $_POST['nombre'];
+	$apepat = $_POST['apepat'];
+	$apemat = $_POST['apemat'];
+	include("conexion.php");
+	//Aqui se revisa si el alumno ya estaba registrado
+	$consultaAlumno=mysqli_query($conexion,"SELECT clave FROM datospersonales WHERE curp='$curp' OR (nombre='$nombre' AND apepat='$apepat' AND apemat='$apemat')");
+	if(mysqli_num_rows($consultaAlumno)>0){
+		echo "<h3 style='color: red;'>El Alumno ya Existe</h3>";
 		echo "<a href='../HTML/registroAlumno.html'>Volver al registro</a>";
-	}	
+	//Fin
+	}else{
+		//Aquí se insertan los datos personales del alumno
+		$genero = $_POST['genero'];
+		$fnac = $_POST['fnac'];
+		$direccion = $_POST['direccion'];
+		$telalumno = $_POST['telcel'];
+		$telcasa = $_POST['telcasa'];
+		$msg="";
+		($genero==0 ? $genero="M" : $genero="F");
+		$registroAlumno=mysqli_query($conexion,"INSERT INTO datospersonales (curp,genero,apepat,apemat,nombre,fnac,direccion,telalumno,telcasa) VALUES ('$curp','$genero','$apepat','$apemat','$nombre','$fnac','$direccion','$telalumno','$telcasa')");
+		//Fin
+		if($registroAlumno){
+			//Aquí se revisa si el directorio de Fotos existe, sí no se crea
+			if (!is_dir('../Fotos')) {
+    			if(!mkdir('../Fotos', 0777)){
+    				$uploadedfileload=false;
+    			}
+			}
+			//Fin
+			//Aquí se sube la foto al directorio y se inserta la foto en el registro del alumno
+			if($uploadedfileload==true){
+				$file_name=$apepat." ".$apemat." ".$nombre;
+				$add="../Fotos/$file_name";
+				if(move_uploaded_file ($_FILES['uploadedfile']['tmp_name'], $add)){
+					$subirFoto=mysqli_query($conexion,"UPDATE datospersonales SET foto='$add' WHERE (nombre='$nombre' AND apepat='$apepat' AND apemat='$apemat')");
+					if($subirFoto){
+						echo "<h3 style='color: green;'>La Foto se Ha Subido Satisfactoriamente</h3>";
+					}else{
+						echo "<h3 style='color: red;'>No se pudo Subir la Foto</h3>";
+						echo "<h3>Favor de Subir la Foto en el Apartado de Actualización de Datos</h3>";
+					}
+				}else{
+					echo "<h3 style='color: red;'>No se pudo Subir la Foto</h3>";
+					echo "<h5>Favor de Subir la Foto en el Apartado de Actualización de Datos</h5>";
+				}
+			}
+			//Fin
+			//Aquí se busca la clave del alumno para asociarlo al resto de los datos
+			$buscarAlumno=mysqli_query($conexion,"SELECT clave FROM datospersonales WHERE nombre='$nombre' AND apepat='$apepat' AND apemat='$apemat'");
+			if($buscarAlumno){
+				$grado = $_POST['grado'];
+				$finscrip = $_POST['finscrip'];
+				$info = mysqli_fetch_array($buscarAlumno);
+				$clave = $info['clave'];
+				$darDeAlta = mysqli_query($conexion,"INSERT INTO altas (clave,finscrip,grado) VALUES ('$clave','$finscrip','$grado')");
+				if(!$darDeAlta){
+					echo "<h3>Favor de subir el Grado y la fecha de Inscripcion en el Apartado de Actualización de Datos</h3>";
+				}
+			}else{
+				echo "<h3>Favor de subir el Grado y la Fecha de Inscripcion en el Apartado de Actualización de Datos</h3>";
+			}
+			//Fin
+			echo "<h3 style='color: green;'>Alumno Registrado con exito</h3>";
+			echo "<a href='../HTML/registroAlumno.html'>Volver al registro</a>";
+		}else{
+			echo "<h3 style='color: red;'>Alumno No Registrado</h3>";
+			echo "<a href='../HTML/registroAlumno.html'>Volver al registro</a>";
+		}
+	}
 ?>
